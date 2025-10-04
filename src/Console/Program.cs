80f8e1f3916a2d37dff78ce91dbaec2ad10686d5
Joe Trumbull <trumbull.j@gmail.com>
2025-10-04 14:08:23 -0500

    Add configuration support to Console project
    Introduced appsettings.json and environment variable configuration to the Console project. Updated .editorconfig for JSON indentation. Added necessary NuGet packages to Infrastructure for configuration and DI support.

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, Lucy!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+    .AddEnvironmentVariables(prefix: "LUCY_")
+    .Build();
